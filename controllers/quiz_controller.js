290f87dede2e822d839e46fe17dfa94403d786df
gpalomares <gpalomares@iesoretania.es>
2015-07-29 00:46:14 +0200

    modulo7 antes heroky
    antes de sumar las ramas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,3 +16,22 @@ exports.answer = function (req, res){
 	}
 })
 };
+
+exports.search = function(req, res){
+	if (req.query.search){
+		var search = req.query.search;
+		search = search.split(" ").join('%');
+		search = '%' + search + '%';
+		query = {
+			where : ["lower(pregunta) like lower(?)", search],
+			order : "pregunta ASC"
+		};
+		models.Quiz.findAll({where:["pregunta like ?", search]}).then(function(quizes){
+			console.log(quizes);
+			res.render('mostrar', {mensaje: quizes});
+		})
+	}
+	else{
+		res.render("search", {mensaje: "holass"});
+	}
+};
--- models/models.js
+++ models/models.js
@@ -1,12 +1,35 @@
 var path = require ("path");
 
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user 	= (url[2] || null);
+var pwd 	= (url[3] || null);
+var protocol= (url[1] || null);
+var dialect = (url[1] || null);
+var port 	= (url[5] || null);
+var host 	= (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 //cargar modelo
 var Sequelize=require("sequelize");
 
+// Reemplazamos la definicion de SQLite por nuestro modelo dinamico
+var sequelize = new Sequelize(DB_name, user, pwd, {
+	dialect : protocol,
+	protocol: protocol,
+	port 	: port,
+	host 	: host,
+	storage : storage, 	// solo SQLite (.env)
+	omitNull: true 		// definido solo para Heroku PostgreSQL
+});
+
+
+
 //usar BBDD
-var sequelize = new Sequelize(null, null, null,
-	{dialect: "sqlite", storage: "quiz.sqlite"}
-	);
+//var sequelize = new Sequelize(null, null, null,
+//	{dialect: "sqlite", storage: "quiz.sqlite"}
+//	);
+
 
 //importar la definición de la tabla
 var Quiz = sequelize.import(path.join(__dirname, "quiz"));
@@ -16,7 +39,7 @@ exports.Quiz = Quiz;
 sequelize.sync().then(function(){
 	Quiz.count().then(function(count){
 		if (count ===0){
-			Quiz.create({pregunta: "capital de Italiz",
+			Quiz.create({pregunta: "capital de Italia",
 							respuesta: "Roma"
 						})
 			.then(function(){console.log("Base de datos inicializada")});
